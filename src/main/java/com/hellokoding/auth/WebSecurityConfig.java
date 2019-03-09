be5ce4c8293c3de8341db054edd7027ed96f4f2b
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2019-03-09 13:15:44 -0300

    atualização portfólio

--- src/main/java/com/hellokoding/auth/WebSecurityConfig.java
+++ src/main/java/com/hellokoding/auth/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/registration","/produtos","/index","/conta","/cadastro").permitAll()
+                    .antMatchers("/resources/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
